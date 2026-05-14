a181c9d66cb2113d1476c8915255f0ea10f8b14a
Shade-Angel <maratsamat18@gmail.com>
2026-05-14 14:47:44 +0500

    создание компонетнов и состояний

--- client/App.tsx
+++ client/App.tsx
@@ -1,14 +1,17 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
 import Navigation from '@/navigation/Navigation'
+import AuthProvider from '@/providers/auth/AuthProvider'
 
 
 export default function App() {
 	return (
 		<>
-			<SafeAreaProvider>
-				<Navigation />
-			</SafeAreaProvider>
+			<AuthProvider>
+				<SafeAreaProvider>
+					<Navigation />
+				</SafeAreaProvider>				
+			</AuthProvider>
 			<StatusBar style='auto'></StatusBar>
 		</>
 	)
