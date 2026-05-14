d42373a98eb72244657a7f32b6e04427ab8aa41d
Shade-Angel <maratsamat18@gmail.com>
2026-05-14 12:35:49 +0500

    создание экрана регистрации

--- client/app/components/screens/auth/Auth.tsx
+++ client/app/components/screens/auth/Auth.tsx
@@ -1,20 +1,51 @@
-import { useTypedNavigation } from "@/hooks/useTypedNavigation";
-import { FC } from "react";
+import Button from "@/components/ui/button/Button";
+import Loader from "@/components/ui/Loader";
+import { IAuthFormData } from "@/types/auth.interface";
+import { FC, useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Pressable, Text, View } from "react-native";
 
 const Auth: FC = () => {
-    const { navigate } = useTypedNavigation()
+    const [isReg, setIsReg] = useState(false)
+
+    const {handleSubmit, reset, control} = useForm<IAuthFormData>({
+        mode: 'onChange'
+    })
+
+    const onSumbit:SubmitHandler<IAuthFormData> = data => {
+        console.log(data)
+    }
+
+    const isLoding = false
 
     return (
-        <View>
-            <Text className="bg-red-900">
-                Auth
-            </Text>
-            <Pressable onPress={() => navigate('Home')}>
-                <Text >
-                    Go to Home
-                    </Text>
-                </Pressable>
+        <View className="mx-2 items-center justify-center h-full">
+            <View className="w-9/12">
+                <Text className="text-center text-black text-3xl font-medium mb-8">
+                    {isReg ? 'Sing Up' : 'Login'}
+                </Text>
+                {isLoding ? (
+                    <Loader /> 
+                ) : (
+                    <> 
+                    
+                        <Button onPress={handleSubmit(onSumbit)}>
+                            {isReg ? 'Sing Up' : 'Login'}
+                        </Button>
+
+                        <Pressable onPress={() => setIsReg(!isReg)}>
+                            <Text className="text-black text-center text-base mt-6">
+                                {isReg
+                                    ? 'Already have an account ' 
+                                    : "Don't have an account? "}
+                                <Text className="text-[#47AA52]">
+                                    {isReg ? 'Login' : 'Sign up'}
+                                </Text>
+                            </Text>
+                        </Pressable>
+                    </>
+                )}
+            </View>
         </View>
     )
 }
---
+++ client/app/components/ui/button/Button.tsx
@@ -0,0 +1,18 @@
+import { FC, PropsWithChildren } from "react";
+import { Pressable, Text } from "react-native";
+import cn from 'clsx'
+import { IButton } from "./button.interface";
+
+const Button: FC<PropsWithChildren<IButton>> = ({ children, className, ...rest }) => {
+    return (
+        <Pressable className={cn(
+            'self-center mt-3.5 bg-[#47AA52] w-full py-3 font-light rounded-lg', className
+            )}
+            {...rest}
+        >
+            <Text className="text-white text-center font-medium text-lg">{children}</Text>
+        </Pressable>
+    )
+}
+
+export default Button
