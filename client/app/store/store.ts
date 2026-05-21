9d9c251d5803f0a00082f532c4f01e01436c5897
Shade-Angel <maratsamat18@gmail.com>
2026-05-21 18:27:20 +0500

    создание корзины

---
+++ client/app/store/store.ts
@@ -0,0 +1,39 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { FLUSH, PAUSE, PERSIST, PersistConfig, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from 'redux-persist'
+
+const persistConfig: PersistConfig<any> = {
+    key: 'root',
+    storage: AsyncStorage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
+    cart: cartSlice.reducer
+})
+
+const persistedReducer = persistReducer<TypeRootState>(
+    persistConfig,
+    rootReducer
+)
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER
+                ]
+            }
+        })
+})
+
+export const persistor = persistStore(store)
+
+export type TypeRootState = ReturnType<typeof rootReducer>
